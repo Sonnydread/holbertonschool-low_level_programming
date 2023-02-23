da08d94a2bfedc78be12441152a3d418e47fd1de
Sonnydread <alvaradoguille590@gmail.com>
2023-02-23 08:46:05 -0800

    3pri

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -14,10 +14,10 @@ int main(void)
 
 char low;
   
-for (low = 'a'; low >= 'z'; low++)
+for (low = 'a'; low <= 'z'; low++)
 putchar(low);
 
-for (low = 'A'; low >= 'Z'; low++)
+for (low = 'A'; low <= 'Z'; low++)
 putchar(low);
 putchar('\n');
 
