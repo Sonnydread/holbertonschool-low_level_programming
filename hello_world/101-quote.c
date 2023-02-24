0f8ce77641872cccff73c9bee2c44d0af6565313
Sonnydread <alvaradoguille590@gmail.com>
2023-02-23 20:59:39 -0800

    quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <unistd.h>
 
-/**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followed by a newline, to standard error.
- * Return: Always 1.
+
+/**
+ * main - and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: Always 1
  */
 
+
 int main(void)
+
 
 {
 
