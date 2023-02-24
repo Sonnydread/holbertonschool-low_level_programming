0d3ef6877131545e2073221cdfd8fe1b1b04b560
Sonnydread <alvaradoguille590@gmail.com>
2023-02-23 19:48:57 -0800

    8pbase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Hexadecimal
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+
+{
+
+char i;
+ 
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+
+for (i = 97; i <= 102; i++)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+
+}
