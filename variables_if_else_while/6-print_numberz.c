fa4b40cd0a42a1edf4fa95f6a7dd75b239ea74e8
Sonnydread <alvaradoguille590@gmail.com>
2023-02-23 18:20:09 -0800

    6pinum

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
- * main - Los numberz 
+ * main - Los numberz
  * Return: Always 0 (Success)
  */
 
@@ -13,7 +13,7 @@ int main(void)
 
 int d;
 
-for (d = '0'; d <= '9';d++)
+for (d = '0'; d <= '9';d++);
 putchar(d);
 putchar('\n');
 
