7926c37cdec730e22b5e5427d5c793539f9dd696
Sonnydread <alvaradoguille590@gmail.com>
2023-02-25 05:43:50 -0800

    isab

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - Imprimir el alfabeto
+ * caracter para chequear
+ * Return: 1 para el afabeto o 0 para lo demás
+ */
+
+int _isalpha(int c)
+
+{
+
+if ((c >= 65 && c <= 90) || (c >= 97 && c <=122))
+{
+return (1);
+}
+return (0);
+
+}
