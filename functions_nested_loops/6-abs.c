651ba9b4496351ed412cf4a8f2e075990961f407
Sonnydread <alvaradoguille590@gmail.com>
2023-02-25 08:27:20 -0800

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Valor absoluto
+ * @c: El num
+ * Return: valor absoluto del zero
+ */
+
+int _abs(int c)
+
+{
+
+if (c < 0)
+{
+int abs_val;
+
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
+
+}
