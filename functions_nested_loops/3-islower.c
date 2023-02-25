e60c41e7d2b773443c0f33e127933f5fd0543811
Sonnydread <alvaradoguille590@gmail.com>
2023-02-25 04:49:56 -0800

    3islo

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - En minúsculas
+ * Cambia de carater
+ * Return: 1 para minúscula o 0 para las demás
+ */
+
+int _islower(int c)
+
+{
+
+if (c >= 97 && c <=122)
+{
+      
+return (1);
+}
+return (0);
+
+}
