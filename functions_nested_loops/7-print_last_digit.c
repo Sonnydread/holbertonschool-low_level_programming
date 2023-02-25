7e83ec3a8f03e858d7c92b7a8fdad2ab25fe32fd
Sonnydread <alvaradoguille590@gmail.com>
2023-02-25 08:42:17 -0800

    lastdig

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * prints the last digit - Imprime el último num
+ * @n: el num
+ * Return: valor del último num
+ */
+
+int print_last_digit(int n)
+
+{
+
+int last;
+
+last = n % 10;
+if (last < 0)
+{
+last = last * -1;
+}
+_putchar(last + '0');
+return (last);
+
+}
