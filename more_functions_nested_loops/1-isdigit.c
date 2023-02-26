863a7de82c9357f08dff82cff999673254dcb1e6
Sonnydread <alvaradoguille590@gmail.com>
2023-02-25 21:10:17 -0800

    digicel

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - Verifica si el carac es un num
+ * @x: el num
+ * Return: 1 para el dígito  y 0 para otros
+ */
+
+int _isdigit(int x)
+
+{
+
+if (x >= 48 && x <= 57)
+{
+return (1);
+}
+return (0);
+      
+}
+ 
