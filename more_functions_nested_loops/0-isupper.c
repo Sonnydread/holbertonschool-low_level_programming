efc76ce0753f75e6ef8ad9282ddfde394ab87e86
Sonnydread <alvaradoguille590@gmail.com>
2023-02-25 20:41:23 -0800

    isup

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - Revisar la letra
+ * @c: Atención al num
+ * Return: 1 para el alto y 0 para otros
+ */
+
+int _isupper(int c)
+
+{
+
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+
+}
