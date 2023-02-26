67fe4563e02ada89b781c2dff647ed2e6cd1e1c1
Sonnydread <alvaradoguille590@gmail.com>
2023-02-25 19:17:03 -0800

    100

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,22 +1,44 @@
 #include "main.h"
 
-
 /**
- * Print_alphabet - Imprimir el alfabeto
- * @
- * Return: void
+ * print_times_table - Imprimir la tabla
+ * starting with 0.
+ * @n: Valor de la tabla
  */
 
-void print_alphabet(void)
+void print_times_table(int n)
 
 {
 
-  char c;
+int num, mult, prod;
 
-  for (c = 'a'; c <= 'z'; c++)
-    {
-      _putchar(c);
-    }
-  _putchar('\n);
+if (n >= 0 && n <= 15)
+{
+for (num = 0; num <= n; num++)
+{
+_putchar('0');
+for (mult = 1; mult <= n; mult++)
+{
+_putchar(',');
+_putchar(' ');
+prod = num * mult;
+if (prod <= 99)
+_putchar(' ');
+if (prod <= 9)
+_putchar(' ');
+if (prod >= 100)
+{
+_putchar((prod / 100) + '0');
+_putchar(((prod /10)) % 10 + '0');
+}
+else if (prod <= 99 && prod >= 10)
+{
+_putchar((prod / 10) + '0');
+}
+_putchar((prod % 10) + '0');
+}
+_putchar('\n');
+}
+}
 
 }
