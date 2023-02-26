943323db4c80e94389be805f086e32da5d1430fe
Sonnydread <alvaradoguille590@gmail.com>
2023-02-26 05:40:16 -0800

    6lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: num de líneas 
+ * Return: empty
+ */
+
+void print_line(int n)
+
+{
+
+int x;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < n; x++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
+
+}
