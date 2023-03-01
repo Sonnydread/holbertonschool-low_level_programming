efac5a7a7b61e77c831ddb4162a3c7b931ff7d44
Sonnydread <alvaradoguille590@gmail.com>
2023-03-01 05:18:36 -0800

    strl

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @str: el string
+ * Return: len of the string
+ */
+
+size_t _strlen(const char *str)
+
+{
+
+size_t length = 0;
+
+while (*str++)
+length++;
+return (length);
+
+{
