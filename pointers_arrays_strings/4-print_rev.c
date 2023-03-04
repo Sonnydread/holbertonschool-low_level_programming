bc727b34ae0a1b1269d4f4d3a7bb333ff999bab3
Sonnydread <alvaradoguille590@gmail.com>
2023-03-03 20:38:33 -0800

    4pri

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.H>
+
+/**
+ * print_rev - print string in reverse
+ * @s: el string
+ * Return: void
+ */
+
+void print_rev(char *s)
+
+{
+  
+int c = 0;
+while (s[c] != '\0'
+{
+c++;
+}
+
+for (c -= 1; c >= 0; c--)
+{
+_putchar(s[c]);
+}
+_putchar('\n');
+
+}
