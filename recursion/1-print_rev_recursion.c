791ff85b5bec75fa1bbc19335521b906a9841b5a
Sonnydread <alvaradoguille590@gmail.com>
2023-03-09 13:18:46 -0800

    reversa

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - reversa
+ * @s: reversa
+ * Return: nada
+ */
+
+void _print_rev_recursion(char *s)
+
+{
+
+if (*s == '\0')
+{
+return;
+}
+
+s++;
+_print_rev_recursion(s);
+s--;
+_putchar(*s);
+}
