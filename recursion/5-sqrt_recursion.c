66889939a4621ff458a40e4d78b5aa82eceb74e9
Sonnydread <alvaradoguille590@gmail.com>
2023-03-09 17:43:39 -0800

    raiz

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+int _sqrt(int n, int i);
+
+/**
+ * _sqrt_recursion - regresa el num de la raiz
+ * @n: num a calcular
+ *
+ * Return: num natural de la raiz
+ */
+int _sqrt_recursion(int n)
+{
+return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt - calcula  el num de la raiz
+ * @n: num a calcular
+ * @i: inter num
+ *
+ * Return: num natural de la raiz
+ */
+int _sqrt(int n, int i)
+{
+int sqrt = i * i;
+
+if (sqrt > n)
+return (i);
+
+if (sqrt == n)
+return (i);
+
+return (_sqrt(n, i + 1));
+}
