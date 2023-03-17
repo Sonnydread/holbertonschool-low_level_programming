a7dfaa062a463848b10fa17cdb6148c3134ab08f
Sonnydread <alvaradoguille590@gmail.com>
2023-03-16 20:24:57 -0700

    1mall

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,52 +2,48 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - shall point
- * @s1: valor 1
- * @s2: valor 2
- * @n: otro valor
+ * string_nconcat - conecta 2 cadenas
+ * @s1: valor1
+ * @s2: valor2
+ * @n: el valor
+ *
  * Return: resultado
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
 
-unsigned int i = 0, j = 0, k = 0, l = 0;
-char *str;
+{
+char *a;
+unsigned int b, c, d;
 
 if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-
-while (s1[i])
-i++;
-
-while (s2[k])
-k++;
-
-if (n >= k)
-l = i + k;
+{
+b = 0;
+}
 else
-l = i + n;
-
-str = malloc(sizeof(char) * l + 1);
-if (str == NULL)
+{
+for (b = 0; *(s1 + b) != '\0'; b++)
+;
+}
+if (s2 == NULL)
+{
+c = 0;
+}
+else
+{
+for (c = 0; *(s2 + c) != '\0'; c++)
+;
+if (n < c)
+c = n;
+}
+c += b;
+a = malloc(sizeof(char) * (c + 1));
+if (a == NULL)
 return (NULL);
-
-k = 0;
-while (j < l)
-{
-if (j <= 1)
-str[j] = s1[j];
-
-if (j >= i)
-{
-str[j] = s2[k];
-k++;
-}
-j++;
-}
-str[j] = '\0';
-return (str);
+for (d = 0; d < b; d++)
+a[d] = *(s1 + d);
+for (b = 0; d < c; b++)
+a[d++] = *(s2 + b);
+a[d] = '\0';
+return (a);
 }
