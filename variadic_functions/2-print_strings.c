c534ed0cb9bd44081858d3d70d0de1cf08168375
Sonnydread <alvaradoguille590@gmail.com>
2023-03-26 05:40:42 -0700

    2pristr

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - print element
+ * @separator: el string
+ * @n: num de strings
+ * @...: La variable
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+
+{
+va_list strings;
+char *str;
+unsigned int index;
+
+va_start(strings, n);
+
+for (index = 0; index < n; index++)
+{
+str = va_arg(strings, char *);
+
+if (str == NULL)
+printf("(nill)");
+
+else
+printf("%s", str);
+
+if (index != (n - 1) && separator != NULL)
+printf("%s", separator);
+}
+printf("\n");
+va_end(strings);
+}
