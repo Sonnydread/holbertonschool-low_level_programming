30e600d248feb20c9b02d3d07c7e2cc58aa3d94a
Sonnydread <alvaradoguille590@gmail.com>
2023-04-13 06:43:00 -0700

    lsuma

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - suma de data
+ * @head: Address 
+ * Return: la suma
+ */
+
+int sum_dlistint(dlistint_t *head)
+
+{
+int sum = 0;
+
+for (; head; head = head->next)
+sum += head->n;
+
+return (sum);
+}
